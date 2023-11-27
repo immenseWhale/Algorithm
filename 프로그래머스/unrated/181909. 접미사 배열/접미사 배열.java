73065d2cbefcec502f13e506b792f76c2c2899bb
immenseWhale <123797393+immenseWhale@users.noreply.github.com>
2023-11-28 07:48:06 +0900

    [level unrated] Title: 접미사 배열, Time: 0.24 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+
+class Solution {
+    public String[] solution(String my_string) {
+       int length = my_string.length();
+        String[] suffixes = new String[length];
+
+        // 모든 접미사를 배열에 저장
+        for (int i = 0; i < length; i++) {
+            suffixes[i] = my_string.substring(i);
+        }
+
+        // 배열을 사전순으로 정렬
+        Arrays.sort(suffixes);
+
+        return suffixes;
+    }
+}
