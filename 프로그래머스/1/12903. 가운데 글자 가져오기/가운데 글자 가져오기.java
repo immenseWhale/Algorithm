c79a9fb040194cb4d54547caf6262ade6b44ef56
immenseWhale <123797393+immenseWhale@users.noreply.github.com>
2024-01-08 19:11:38 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public String solution(String s) {
+        int length = s.length();
+
+        if (length % 2 == 0) {
+            return s.substring(length / 2 - 1, length / 2 + 1);
+        } else {
+            return String.valueOf(s.charAt(length / 2));
+        }
+    }
+}
