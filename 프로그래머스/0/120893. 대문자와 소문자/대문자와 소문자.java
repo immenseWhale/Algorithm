b766d4fccabd81569133cd6668256568ae5a5699
immenseWhale <123797393+immenseWhale@users.noreply.github.com>
2024-01-26 22:46:06 +0900

    [level 0] Title: 대문자와 소문자, Time: 1.02 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder result = new StringBuilder();
+
+        for (char c : my_string.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                result.append(Character.toLowerCase(c));
+            } else if (Character.isLowerCase(c)) {
+                result.append(Character.toUpperCase(c));
+            } else {
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+    }
+}
