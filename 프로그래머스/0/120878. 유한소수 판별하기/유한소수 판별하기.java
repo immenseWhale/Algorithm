6f221920b0809abb7cfa5da1817cae44190fdeb3
immenseWhale <123797393+immenseWhale@users.noreply.github.com>
2024-02-11 09:38:04 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.02 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int a, int b) {
+        int gcd = gcd(a, b);
+        a /= gcd;
+        b /= gcd;
+        
+        while (b % 2 == 0) {
+            b /= 2;
+        }
+        while (b % 5 == 0) {
+            b /= 5;
+        }
+        
+        return (b == 1) ? 1 : 2;
+    }
+    
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+}
